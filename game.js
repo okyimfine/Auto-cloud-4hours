bc1e704326a166f6ed7ff299ed48b0defc96caa1
AeoxHub <163515239+okyimfine@users.noreply.github.com>
2025-05-31 18:28:12 +0800

    Delete game.js

--- game.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  alert("Thank you! Reward processed.\nEmail sent to pleasegithub7@gmail.com");
-});
-
-const backBtn = document.getElementById("backToMain");
-backBtn.addEventListener("click", () => {
-  window.location.href = "https://okyimfine.github.io/Free-cloud-ugphone/";
-});
