126425fe305938aefea8303137f348c03dd5fa1d
AeoxHub <163515239+okyimfine@users.noreply.github.com>
2025-05-31 18:22:59 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  alert("Thank you! Reward processed.\nEmail sent to pleasegithub7@gmail.com");
+});
+
+const backBtn = document.getElementById("backToMain");
+backBtn.addEventListener("click", () => {
+  window.location.href = "https://yourusername.github.io/fake-cloud-page/";
+});
